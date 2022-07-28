d12eb4fc34a62bc6ac387706a8c661a8f3ecc79c
Prathibha A M <45116350+prathiba9740@users.noreply.github.com>
2022-07-28 23:20:38 +0530

    Add files via upload

---
+++ Assignment4thDay_PAM.js
@@ -0,0 +1,5 @@
+function clickMe(){
+    document.querySelector(".screen").style.backgroundColor = "green";
+    document.querySelector(".screen").style.height = '100vh';
+    document.querySelector("h1").style.display ="none";
+}
